c7e5cea90bffab30e2df4a2a3ef477da1890c8a6
Krzysztof Oporowski <krzysztof.oporowski@gmail.com>
2019-07-22 21:02:45 +0200

    New functionalities
    1. Method set_sl in Transaction class - to define the stop loss for open transaction
    2. Function get_date_only - extracts date part of the index value from Quandl data
    3. Function process_data - function containing the logic of transactions, to be applied to the DataFrame containing stock data
    4. Updated code according to pylint hints
    5. File transakcje.csv are the result of  exporting the transactions DataFrame to the CSV file

--- stock_sys_checker.py
+++ stock_sys_checker.py
@@ -5,13 +5,15 @@ Created on Wed Jul 10 19:48:17 2019
 @author: krzysztof.oporowski
 """
 
-import quandl
 import pickle
-import pandas as pd
+import numbers
 from datetime import date
+import quandl
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def get_own_data(equity_name, quandl_API_token):
+def get_own_data(equity_name, quandl_api_token):
     '''
     Function read data of the signle quity using the Quandl. It requires the
     Quandl API to be provided, to make sure that more than 50 queries are
@@ -36,7 +38,7 @@ def get_own_data(equity_name, quandl_API_token):
             data = pickle.load(opened_file)
         print('data from file {} used'.format(opened_file))
     except:
-        quandl.ApiConfig.api_key = quandl_API_token
+        quandl.ApiConfig.api_key = quandl_api_token
         # for equity_name in equity_names:
         quandl_query = 'WSE/' + equity_name
         data = quandl.get(quandl_query)
@@ -58,11 +60,11 @@ def get_own_data(equity_name, quandl_API_token):
 
 def define_gl(gl):
     '''
-    Function returns Pandas dataframe where all transactions are stored 
+    Function returns Pandas dataframe where all transactions are stored
     '''
-    cols = ['id', 'stocks_number', 'open_price', 'open_value', 
-            'open_commission', 'open_total', 'close_price', 'close_value',
-            'close_commission', 'close_total']
+    cols = ['id', 'stocks_number', 'open_price', 'open_value',
+            'open_commission', 'open_total', 'SL', 'close_price',
+            'close_value', 'close_commission', 'close_total', 'trans_result']
     transactions = pd.DataFrame(gl, columns=cols)
     return transactions
 
@@ -79,7 +81,7 @@ class Budget:
         '''
         self.equity = amount
         self.free_margin = amount
-        
+
     def manage_amount(self, change_amount):
         '''
         Method used to manage the equity changes as new transactions appears.
@@ -95,8 +97,7 @@ class Transaction:
     Class used to store transaction data and provide methods to manage all
     transactions
     '''
-    #trans_number = 0
-    
+
     def __init__(self, trans_numb, transaction_gl, comm=0.0039):
         '''
         Parameters:
@@ -104,23 +105,26 @@ class Transaction:
         comm - broker's commision. Default value for DM mBank
         trans_numb - externally provided transaction ID
         transation_gl - list to store transactions
-        
         '''
+        # pylint: disable=too-many-instance-attributes
+
         self.stocks_number = 0 # number of stocks bought
         self.open_price = 0 # price for opening the transaction
         self.close_price = 0 # price for closing the transaction
         self.comm_rate = comm # broker's commision
         self.open_value = 0 # worth of transaction without commission
-        self.close_value = 0 
+        self.close_value = 0
         self.comm_open_value = 0 # commision for opening the transaction
         self.comm_close_value = 0 # commision for closing the transaction
         self.open_total = 0 # total value of the stocks price + commision
         self.close_total = 0
+        self.trans_result = 0
+        self.SL = 0 # if below this value, stocks are sold Stop Loss
         self.trans_id = trans_numb # ID of the transaction
         # self.trans_number = self.trans_number + 1 # ID for next transaction
         self.in_transaction = False # transaction indicator
         self.gl = transaction_gl
-    
+
     def open_transaction(self, number, price):
         '''
         Method to buy stocks.
@@ -138,7 +142,43 @@ class Transaction:
                 self.comm_open_value = 3
             self.open_total = self.open_value + self.comm_open_value
             self.in_transaction = True
-        
+            self.register_transaction()
+
+    def set_sl(self, sl_type, sl_factor, price):
+        '''
+        Functions sets the SL on the price.
+        Parameters:
+        -----------
+        sl_type   - string, 2 possibilities
+                      - atr - stop loss based on ATR calculations
+                      - percent - stop loss based on the percentage slippage
+        sl_factor - float, if sl_type = 'atr', than is the ATR value,
+                    if sl_type = 'percent' it is just the value of the
+                    percent, between 0 and 100
+        price     - current price
+        '''
+        if sl_type not in ['atr', 'percent']:
+            print('Value {} of sl_type is not appropriate. Use "atr" or \
+                  "percent" only. Setting SL to 0'.format(sl_type))
+            self.SL = 0
+        elif not isinstance(sl_factor, numbers.Number):
+            print('number of type int or float is expected, not {}'.
+                  format(type(sl_factor)))
+        else:
+            if sl_type == 'atr':
+                pass
+            else:
+                if sl_factor < 0 or sl_factor > 100:
+                    print('sl_factor in percent mode must be 0 -100 value. \
+                          Setting SL to 0 PLN.')
+                    self.SL = 0
+                else:
+                    new_sl = price - price * (sl_factor / 100)
+                    if new_sl > self.SL:
+                        self.SL = new_sl
+                        #print('SL set to {}'.format(self.SL))
+                        self.register_transaction()
+
     def close_transaction(self, price):
         '''
         Method to close the transaction
@@ -150,66 +190,135 @@ class Transaction:
             if self.comm_close_value < 3:
                 self.comm_close_value = 3
             self.close_total = self.close_value - self.comm_close_value
-            self.in_transaction = False
+            self.trans_result = self.close_total - self.open_total
             self.register_transaction()
-            
+            self.reset_values()
+
+    def reset_values(self):
+        '''
+        Function resets all values after the transaction is closed
+        '''
+        self.stocks_number = 0
+        self.open_price = 0
+        self.close_price = 0
+        self.open_value = 0
+        self.close_value = 0
+        self.comm_open_value = 0
+        self.comm_close_value = 0
+        self.open_total = 0
+        self.close_total = 0
+        self.trans_result = 0
+        self.SL = 0
+        self.in_transaction = False
+        self.trans_id = self.trans_id + 1
+
     def register_transaction(self):
+        '''
+        Function registers the transaction details in the general ledger.
+        '''
         print('''
               Transakcja numer: {}, ilosc: {}, cena otwarcia {},
               wartosc otwarcia: {}, prowizja otwarcia: {}, koszt otwarcia {},
-              cena zamkn: {}, wartosc zamkn: {}, prowizja zamkn: {}, 
+              SL: {}, cena zamkn: {}, wartosc zamkn: {}, prowizja zamkn: {}, 
               koszt_zamkn: {}
               '''.format(self.trans_id, self.stocks_number,
-                         self.open_price, self.open_value, 
-                         self.comm_open_value, self.open_total,
-                         self.close_price, self.close_value, 
+                         self.open_price, self.open_value,
+                         self.comm_open_value, self.open_total, self.SL,
+                         self.close_price, self.close_value,
                          self.comm_close_value, self.close_total
                          )
               )
-        '''
-        row = {'id':self.trans_id, 
-               'stocks_number':self.stocks_number,
-               'open_price':self.open_price, 
-               'open_value':self.open_value, 
-               'open_commission':self.comm_open_value, 
-               'open_total':self.open_total,
-               'close_price':self.close_price, 
-               'close_value':self.close_value, 
-               'close_commission':self.comm_close_value, 
-               'close_total':self.close_total
-               }
-        self.gl = self.gl.append(row, ignore_index=True)
-        
-        #= df.append(row, ignore_index=True)
-        '''
         row = [
                self.trans_id, self.stocks_number, self.open_price, 
-               self.open_value, self.comm_open_value, self.open_total,
+               self.open_value, self.comm_open_value, self.open_total, self.SL,
                self.close_price, self.close_value, self.comm_close_value, 
-               self.close_total
+               self.close_total, self.trans_result
                ]
         self.gl.append(row)
 
+def get_date_only(row):
+    '''
+    To process index date/time value from Quandl to get date only
+    '''
+    date_time = row.name
+    date_time = pd.to_datetime(date_time)
+    date_only = date_time.date()
+    return date_only
+
+def process_data(row):
+    '''
+    Define algo to be applied to the DataFrame
+    '''
+    global BUDZET
+    global GL
+    global trans_id
+    global trans
+    global count
+    
+    if not trans.in_transaction:
+        if row['fast_ma'] > row['slow_ma']:
+            #trans.in_transaction = True
+            trans.open_transaction(10, row['open'])
+            trans.set_sl(sl_type='percent', sl_factor=10, price=row['open'])
+            '''
+            trans_date = get_date_only(row)
+            print(
+                    '{}: fast_ma={}, slow_ma={}'.format(
+                            trans_date, row['fast_ma'], row['slow_ma']
+                            )
+                  )
+            print(trans_id)
+            '''
+            trans_id = trans_id + 1
+    else:
+        #print('in transaction')
+        if row['fast_ma'] < row['slow_ma']:
+            '''print('fast_ma {} lower than slow_ma {}, closing'.format(
+                    row['fast_ma'], row['slow_ma']))'''
+        if trans.SL > row['low']:
+            '''print('price {} lower than sl {}, closing'.format(
+                    row['low'], trans.SL))'''
+            trans.close_transaction(price=trans.SL)
+        else:
+            #print('checking if rise sl')
+            trans.set_sl(sl_type='percent', sl_factor=10, price=row['close'])
+    
 
 # test of Budget and Transactions working
-budzet = Budget()
-gl = []
-for i in range(0,3):
-    trans = Transaction(i, gl)
+BUDZET = Budget()
+GL = []
+trans_id = 0
+count = 0
+trans = Transaction(trans_id, GL)
+'''
+for i in range(0, 3):
+    trans = Transaction(i, GL)
+
     trans.open_transaction(5, 127) # pretending to buy 5 stocks per 127
-    budzet.manage_amount(-trans.open_total)
-    print('moj budzet to: {}'.format(budzet.equity))
+    if i == 0:
+        trans.set_sl(sl_type='percent', sl_factor=10, price=trans.open_price)
+    BUDZET.manage_amount(-trans.open_total)
+    print('moj BUDZET to: {}'.format(BUDZET.equity))
     trans.close_transaction(125) # pretending to sell 5 stocks per 125
-    budzet.manage_amount(trans.close_total)
-    print('moj budzet to: {}'.format(budzet.equity))
-transactions = define_gl(gl)
-print(transactions)
-
+    BUDZET.manage_amount(trans.close_total)
+    print('moj budzet to: {}'.format(BUDZET.equity))
+TRANSACTIONS = define_gl(GL)
+print(TRANSACTIONS)
+'''
 # test of get_own_data working
-data = get_own_data('Amica', '8zKzKFh-8eePuNy9wpuP')  # , 'Pekao'
-data['fast_ma'] = data['close'].rolling(10).mean()
-data['slow_ma'] = data['close'].rolling(30).mean()
-data['close'].loc['2019-01-03':].plot()
-data['fast_ma'].loc['2019-01-03':].plot()
-data['slow_ma'].loc['2019-01-03':].plot()
+DATA = get_own_data('Amica', '8zKzKFh-8eePuNy9wpuP')  # , 'Pekao'
+DATA['fast_ma'] = DATA['close'].rolling(10).mean()
+DATA['slow_ma'] = DATA['close'].rolling(30).mean()
+DATA.dropna()
 
+
+# apply algo to the stock (DataFrame)
+DATA.apply(process_data, axis=1)
+
+transactions = define_gl(GL)
+print(transactions)
+#print('trans_id = {}'.format(trans_id))
+#DATA['close'].loc['2019-01-03':].plot()
+#DATA['fast_ma'].loc['2019-01-03':].plot()
+#DATA['slow_ma'].loc['2019-01-03':].plot()
+#plt.show()
